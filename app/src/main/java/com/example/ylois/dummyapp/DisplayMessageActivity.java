7b51a8ac79b61e6cbad8c95793760a2fefea3e10
Yloidan <aarfelk@gmail.com>
2015-04-21 17:32:26 +0300

    2.1 AsynTask Introduced and WORKING !

--- app/src/main/java/com/example/ylois/dummyapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/ylois/dummyapp/DisplayMessageActivity.java
@@ -1,11 +1,11 @@
 package com.example.ylois.dummyapp;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
@@ -24,10 +24,85 @@ public class DisplayMessageActivity extends ActionBarActivity {
 
         TextView textView = new TextView(this);
         textView.setTextSize(14);
-        textView.setText(message);
+
+        /* DEBUGGING
+
+        // Construct a JSONObject from a source JSON text string.
+
+        // A JSONObject is an unordered collection of name/value pairs. Its external
+
+        // form is a string wrapped in curly braces with colons between the names
+
+        // and values, and commas between the values and names.
+
+        try {JSONObject jo = new JSONObject(message);
+            JSONArray ja;
+ //           jo = jo.getJSONObject("ResultSet");
+            ja = jo.getJSONArray("entity");
+            int resultCount = ja.length();
+ //           String  myArray[]=new String[resultCount];
+            StringBuilder sb = new StringBuilder();
+            boolean appendSeparator = false;
+            for (int i = 0; i < resultCount; i++)
+
+            {
+                JSONObject resultObject = ja.getJSONObject(i);
+               String name = resultObject.getString("content");
+
+                if (appendSeparator)
+                    sb.append(','); // a comma
+                appendSeparator = true;
+                sb.append(name);
+
+
+//               sb.append(ja.get(i));
+//               myArray[i]=name;
+//               TextView.append(myArray[i]);
+//               TextView.append("\n");
+
+
+            }
+
+            textView.setText(sb.toString());
+
+
+        }
+        catch ( JSONException e2){
+            System.err.println("JSONException " + e2.getMessage());
+        }
+
+DEBUGGING   */
+      textView.setText(message);
+
+
+        // A JSONArray is an ordered sequence of values. Its external form is a
+
+        // string wrapped in square brackets with commas between the values.
+
+
+
+// Get the JSONObject value associated with the search result key.
+
+
+
+// Get the JSONArray value associated with the Result key
+
+
+// Get the number of search results in this set
+
+
+
+// Loop over each result and print the title, summary, and URL
+
+
+
+
+
+
+
+
 
         //setting textview as the root view of the activity’s layout
-
         setContentView(textView);
 
      //not needed
